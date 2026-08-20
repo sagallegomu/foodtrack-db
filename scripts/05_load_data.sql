bd3bac3e8551712a7e028fe446db272515f06f57
sagallegomu <sagamunoz05@gmail.com>
2026-08-20 11:18:59 -0500

    fix: remove unsupported Linux CODEPAGE option

--- scripts/05_load_data.sql
+++ scripts/05_load_data.sql
@@ -7,27 +7,27 @@ USE FoodTrack;
 
 BULK INSERT dbo.foodtrucks
 FROM '/var/opt/mssql/import/foodtrucks.csv'
-WITH (FORMAT = 'CSV', FIRSTROW = 2, FIELDQUOTE = '"', CODEPAGE = '65001', TABLOCK);
+WITH (FORMAT = 'CSV', FIRSTROW = 2, FIELDQUOTE = '"', TABLOCK);
 
 BULK INSERT dbo.products
 FROM '/var/opt/mssql/import/products.csv'
-WITH (FORMAT = 'CSV', FIRSTROW = 2, FIELDQUOTE = '"', CODEPAGE = '65001', TABLOCK);
+WITH (FORMAT = 'CSV', FIRSTROW = 2, FIELDQUOTE = '"', TABLOCK);
 
 BULK INSERT dbo.locations
 FROM '/var/opt/mssql/import/locations.csv'
-WITH (FORMAT = 'CSV', FIRSTROW = 2, FIELDQUOTE = '"', CODEPAGE = '65001', TABLOCK);
+WITH (FORMAT = 'CSV', FIRSTROW = 2, FIELDQUOTE = '"', TABLOCK);
 
 -- Para demostrar el extra de Python, omití este bloque y ejecutá cargar_datos.py.
 -- Ejecutalo solamente si querés cargar orders también con BULK INSERT.
 /*
 BULK INSERT dbo.orders
 FROM '/var/opt/mssql/import/orders.csv'
-WITH (FORMAT = 'CSV', FIRSTROW = 2, FIELDQUOTE = '"', CODEPAGE = '65001', TABLOCK);
+WITH (FORMAT = 'CSV', FIRSTROW = 2, FIELDQUOTE = '"', TABLOCK);
 */
 
 -- Ejecutá este bloque después de cargar orders, ya sea por BULK INSERT o Python.
 /*
 BULK INSERT dbo.order_items
 FROM '/var/opt/mssql/import/order_items.csv'
-WITH (FORMAT = 'CSV', FIRSTROW = 2, FIELDQUOTE = '"', CODEPAGE = '65001', TABLOCK);
+WITH (FORMAT = 'CSV', FIRSTROW = 2, FIELDQUOTE = '"', TABLOCK);
 */
